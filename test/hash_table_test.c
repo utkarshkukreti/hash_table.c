8be7d042774735328d83fe39f3f13064d713154a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-02-15 14:43:53 +0530

    Add test for hash_table_{get,put} with a bad hashing function.

--- test/hash_table_test.c
+++ test/hash_table_test.c
@@ -1,5 +1,8 @@
 #include "test.h"
 
+// http://xkcd.com/221/
+static unsigned int get_random_number(void *a);
+
 void test_hash_table() {
     check_start_group("hash_table_new()", 1);
     {
@@ -29,4 +32,31 @@ void test_hash_table() {
         check("entries_count == 1", a->entries_count == 1);
         check("get i1 == i3", hash_table_get(a, &i1) == &i3);
     }
+
+    check_start_group("hash_table_{get,put} with bad hashing function", 1);
+    {
+        hash_table *a = hash_table_new();
+        a->compare = hash_table_int_compare;
+        a->hash = get_random_number;
+        // key and value can only be pointers
+        int i1 = 10, i2 = 20, i3 = 30;
+        check("entries_count == 0", a->entries_count == 0);
+        check("put i1, i2", hash_table_put(a, &i1, &i2));
+        check("entries_count == 1", a->entries_count == 1);
+        check("put i2, i3", hash_table_put(a, &i2, &i3));
+        check("entries_count == 2", a->entries_count == 2);
+        check("get i2 == i3", hash_table_get(a, &i2) == &i3);
+        check("put i3, i1", hash_table_put(a, &i3, &i1));
+        check("entries_count == 3", a->entries_count == 3);
+        check("get i3 == i1", hash_table_get(a, &i3) == &i1);
+        check("buckets[4]->next != NULL", a->buckets[4]->next != NULL);
+        check("buckets[4]->next->next != NULL",
+                a->buckets[4]->next->next != NULL);
+    }
+}
+
+// http://xkcd.com/221/
+static unsigned int get_random_number(void *a) {
+    return 4; // chosen by fair dice roll.
+              // guaranteed to be random.
 }
